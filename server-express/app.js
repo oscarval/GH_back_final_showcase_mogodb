c6c62fcb6134ea40067b56b5b66f8c81a1661a4f
Alexis <entulinea.alexis@gmail.com>
2020-10-03 22:09:47 +0200

    Add mongoose Add routes

--- server-express/app.js
+++ server-express/app.js
@@ -1,20 +1,29 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Routes
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
+// Moongose
+var mongoose = require("./config/mongoose");
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  // we're connected!
+});
+
+// Express
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Set Routes
+app.use("/users", usersRouter);
 
 module.exports = app;
--- server-express/routes/users.js
+++ server-express/routes/users.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const User = require("../models/user/user");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/login", function (req, res, next) {
+  const user = User.findOne({ email: "req.email", password: req.password });
+  // TODO: Control errors
+  res.send({ prueba: user });
+});
+
+router.get("/register", function (req, res, next) {
+  const user = new User({
+    name: req.name,
+    email: req.email,
+    password: req.password,
+  });
+
+  user
+    .save()
+    .then(async () => {
+      await user.generateAuthToken();
+      await user.save();
+      res.send({ prueba: "saved" });
+    })
+    .catch((error) => {
+      res.send({ prueba: error });
+    });
 });
 
 module.exports = router;
